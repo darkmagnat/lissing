fa63598ad59f3b4850f99ce3004015369498f3ce
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-12 18:26:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', function(){
     client.user.setPresence({
  game: { 
     type: 2,
-     url: 'http://spoti.fi/28XQEzP',
+     url: 'https://open.spotify.com/album/2Ti79nwTsont5ZHfdxIzAm',
     name: '-Universe Town` 1K-Soon........ ',
     application_id: '501452003143319553',//هنا ايدي البوت 
      assets: {
